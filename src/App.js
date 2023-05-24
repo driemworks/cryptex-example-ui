da1e9151548a19812232e9eeff463fd34c91bc27
driemworks <tonyrriemer@gmail.com>
2023-05-24 11:28:56 -0500

    configure external ipfs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* global BigInt */
 import './App.css';
 import { ApiPromise, WsProvider, Keyring } from "@polkadot/api";
 import { web3Accounts, web3Enable, web3FromAddress } from '@polkadot/extension-dapp';
@@ -13,12 +12,9 @@ import SharedData from './components/shared/shared.component';
 
 
 import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
-import Events from './events.component';
-import { Box, Button, TextField } from '@mui/material';
 import TruncatedDisplay from './components/common/truncate-display.component';
 import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import WebIcon from '@mui/icons-material/Web';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDiscord } from '@fortawesome/free-brands-svg-icons';
@@ -35,39 +31,24 @@ function App() {
   const [ipfs, setIpfs] = useState(null);
   const [api, setApi] = useState(null);
 
-  const [doCollapseSidebar, setDoCollapseSidebar] = useState(false);
-
   const [acct, setAcct] = useState(null);
   const [addr, setAddr] = useState('');
   const [signer, setSigner] = useState(null);
 
   const [newIpfsPort, setNewIpfsPort] = useState('5001');
   const [newIpfsHost, setNewIpfsHost] = useState('127.0.0.1');
-  // const [newPort, setNewPort] = useState('9944');
-  // const [newHost, setNewHost] = useState('127.0.0.1');
-  const [blockNumber, setBlockNumber] = useState(0);
 
   const [host, setHost] = useState('127.0.0.1');
   const [port, setPort] = useState('9944');
 
-  const [ipfsPort, setIpfsPort] = useState('5001');
-  const [ipfsHost, setIpfsHost] = useState('127.0.0.1');
+  const [ipfsPort, setIpfsPort] = useState('443');
+  const [ipfsHost, setIpfsHost] = useState('ipfs.idealabs.network');
   const [uri, setUri] = useState('');
 
   useEffect(() => {
     // let provider = new WsProvider(`ws://${host}:${port}`);
     let provider = new WsProvider(`wss://cryptex.idealabs.network:443`);
     const setup = async () => {
-      const ipfs = await create({
-        host: ipfsHost,
-        port: ipfsPort,
-        protocol: 'http',
-      });
-      let id = await ipfs.id();
-      if (id !== null) {
-          setIpfs(ipfs);
-      }
-      setIpfs(ipfs);
       // setup api for blockchain
       const api = await ApiPromise.create({
         provider,
@@ -77,12 +58,13 @@ function App() {
       });
       await api.isReady;
       setApi(api);
+      await setupIpfs();
       // load ALICE account
       const keyring = new Keyring({ type: 'sr25519' });
       // let uriAcct = keyring.addFromUri(uri);
       // returns an array of all the injected sources
       // (this needs to be called first, before other requests)
-      await web3Enable('my cool dapp');
+      await web3Enable('Enigma');
       // returns an array of { address, meta: { name, source } }
       // meta.source contains the name of the extension that provides this account
       const allAccounts = await web3Accounts();
@@ -98,6 +80,23 @@ function App() {
     }
     setup();
   }, [uri]);
+
+  const setupIpfs = async() => {
+    console.log('Establishing a connection to IPFS');
+    const ipfs = await create({
+      host: ipfsHost,
+      port: ipfsPort,
+      protocol: 'https',
+    });
+    let id = await ipfs.id();
+    console.log(id);
+    if (id !== null) {
+        setIpfs(ipfs);
+        // setIpfsReady(true);
+    } else {
+      console.log('ipfs unavailable');
+    }
+  }
   
   const initEventListener = async (api, acct) => {
       // Subscribe to system events via storage
@@ -127,11 +126,28 @@ function App() {
         <div>
           <h2>Welcome!</h2>
         </div>
-        <p>Engima is an experimental multi-party communication app that lets you and your peers collectively secure and authorize access to data.</p>
+        <p>Engima is an experimental, decentralized, multi-party *communication* dapp that lets you and your peers collectively secure and authorize access to data.</p>
         <WarningAmberIcon /> <span>This project is under development (and not mobile friendly). Current messages to be encrypted are <b>limited to 32-bit strings</b>.</span>
         <br></br>
         <h3>Securely share data with your peers</h3>
-        <p>Enigma is a tool that lets you securely share data with your peers. It uses a distributed key generation mechanism to create a shared secret key that only members of the group can decrypt. This means that your data is protected from anyone who is not a member of the group, even if they have access to the encrypted data. This is a 'bring your own storage' solution (see getting started below).</p>
+        <p>Enigma is a tool that lets you securely share data with your peers. It uses a distributed key generation mechanism to create a shared secret key that only members of the group can decrypt. This means that your data is protected from anyone who is not a member of the group, even if they have access to the encrypted data.</p>
+        <h1>
+          Getting Started
+        </h1>
+        <div className='instructions'> 
+          <span>
+            1. Install the <a target="_blank" href='https://polkadot.js.org/extension/'>polkadotjs extension</a> and create a new account, or use your existing account
+          </span>
+          <span>
+            2. Paste your address in our <a target='_blank' href='https://discord.com/channels/920374925984927744/1110259632250835015'>Discord</a> to request some tokens. We are working on automating this process but it's manual for now.
+          </span>
+          <span>
+            3. Use the tabs on the left to create/manage societies and secrets.
+          </span>
+          <span>
+            4. Check out our <a target='_blank' href="https://github.com/ideal-lab5">github</a> to stay up to date on development or to learn how to get more involved. This is an open source project that has been built with passion but without funding or other support. If you'd like to invest in the project or collaborate, send me a message at driemworks@idealabs.network, or share a message with me on Enigma at 5GGrFp7o5b5CMSi9uZnkveRusegbVgVUa5BvqXRQHsJ9SSjc. 
+          </span>
+        </div>
         <h3>How it works</h3>
         <h4>Create or Join a Society</h4>
         <p>To begin, you can either create a new society or join an existing one. A society is a group of individuals who have gone through the distributed keypair generation (DKG) process together. Each society has a public key that serves as an encryption key for data sharing.
@@ -145,23 +161,6 @@ function App() {
         <h4>Collaborate and Access Shared Secrets</h4>
         <p>As a member of a society, you have the opportunity to collaborate and access shared secrets from other trusted members. When a threshold of members has shared their secret with your address, you gain the ability to decrypt the shared secret and unlock its contents.
         </p>
-        <h1>
-          Getting Started
-        </h1>
-        <div className='instructions'> 
-          <span>
-            1. Install the <a target="_blank" href='https://polkadot.js.org/extension/'>polkadotjs extension</a> and create a new account
-          </span>
-          <span>
-            2. Enigma is a 'bring your own storage' solution. Install <a target="_blank" href="https://docs.ipfs.tech/install/">IPFS</a> (connecting to default swarm should work).
-          </span>
-          <span>
-            3. Paste your address in our <a target='_blank' href='https://discord.com/channels/920374925984927744/1110259632250835015'>Discord</a> to request some tokens. We are working on automating this process but it's manual for now.
-          </span>
-          <span>
-            4. Use the tabs on the left to create/manage socieities and secrets.
-          </span>
-        </div>
       </div>
     );
   }
@@ -203,6 +202,35 @@ function App() {
   return (
     <div className="App">
       <div className='header'>
+        {/* <div className='ipfs-config'>
+          <Box sx={{ 
+            width: 500, flexShrink: 0, borderBottom: 1, borderColor: 'divider', backgroundColor: 'primary.secondary', }}>
+              <TextField 
+                label="ipfs port" 
+                id='ipfsPort' 
+                type="number" 
+                value={newIpfsPort} 
+                onChange={e => { setNewIpfsPort(e.target.value) }}
+              />
+              <TextField 
+                label="ipfs host" 
+                id='ipfsHost' 
+                type="text" 
+                value={newIpfsHost} 
+                onChange={e => { 
+                  setNewIpfsHost(e.target.value);
+                }}
+              />
+              <Button onClick={() => {
+                if (newIpfsPort !== '' && newIpfsHost !== '') {
+                  setIpfsPort(newIpfsPort);
+                  setIpfsHost(newIpfsHost);
+                  setupIpfs();
+                  forceUpdate();
+                }
+              }}>Enter</Button>
+            </Box>
+        </div> */}
         <div className='title'>
           <h2 onClick={() => setActiveComponent(<HomePage />)}>
             Enigma
@@ -212,72 +240,24 @@ function App() {
           <span>
             Api is { api === null ? 'not' : '' } ready
           </span>
-          { acct === null ? <div>connect Wallet</div> :
-          <TruncatedDisplay data={acct.address} message="Address: " /> }
+          <span>
+            Ipfs is { ipfs === null ? 'not' : '' } ready
+          </span>
+          { addr === '' ? <div>No wallet detected</div> :
+          <TruncatedDisplay data={addr} message="Address: " /> }
         </div>
       </div>
-      <div className='body'>
-        { ipfs === null ?
-        <div>
-          <div className='config-container'>
-            <Box sx={{ width: 200, flexShrink: 0, borderBottom: 1, borderColor: 'divider' }}>
-              {/* <TextField label="URI" id='uri' type="text" value={newUri} onChange={e => { setNewUri(e.target.value) }} /> */}
-              {/* <Box>
-                <TextField 
-                  label="port" 
-                  id='port' 
-                  type="number" 
-                  value={newPort} 
-                  onChange={e => { setNewPort(e.target.value) }}
-                />
-                <TextField 
-                  label="host" 
-                  id='host' 
-                  type="text" 
-                  value={newHost} 
-                  onChange={e => { setNewHost(e.target.value) }}
-                />
-              </Box> */}
-              <Box>
-                <TextField 
-                  label="ipfs port" 
-                  id='ipfsPort' 
-                  type="number" 
-                  value={newIpfsPort} 
-                  onChange={e => { setNewIpfsPort(e.target.value) }}
-                />
-                <TextField 
-                  label="ipfs host" 
-                  id='ipfsHost' 
-                  type="text" 
-                  value={newIpfsHost} 
-                  onChange={e => { setNewIpfsHost(e.target.value) }}
-                />
-              </Box>
-              <Button onClick={() => {
-                forceUpdate();
-                if (newIpfsPort !== '') {
-                  setIpfsPort(newIpfsPort);
-                }
-                if (newIpfsHost !== '') {
-                  setIpfsHost(newIpfsHost);
-                }
-              }}>Submit</Button>
-            </Box>
-          </div>
-        </div>
-        : <div className='content'>
-          <Sidebar>
-            <Menu>
-              <MenuItem onClick={() => handleSetDisplay('create')}>Create Society</MenuItem>
-              <MenuItem onClick={() => handleSetDisplay('memberships')}>Societies</MenuItem>
-              <MenuItem onClick={() => handleSetDisplay('filesystem')}>View Files</MenuItem>
-              <MenuItem onClick={() => handleSetDisplay('shared')}>Shared With You</MenuItem>
-            </Menu>
-          </Sidebar>
-          { ActiveComponent }
-        </div>
-        }
+      <div className='body'><div className='content'>
+        <Sidebar>
+          <Menu>
+            <MenuItem onClick={() => handleSetDisplay('create')}>Create Society</MenuItem>
+            <MenuItem onClick={() => handleSetDisplay('memberships')}>Societies</MenuItem>
+            <MenuItem onClick={() => handleSetDisplay('filesystem')}>View Files</MenuItem>
+            <MenuItem onClick={() => handleSetDisplay('shared')}>Shared With You</MenuItem>
+          </Menu>
+        </Sidebar>
+        { ActiveComponent }
+      </div>
       </div>
       <div className='footer'>
           <a target='_blank' href="https://github.com/ideal-lab5"><GitHubIcon /></a>
